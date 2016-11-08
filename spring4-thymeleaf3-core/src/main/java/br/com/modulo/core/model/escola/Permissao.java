3a5bb97d6ac48920c0b1feed69381fd27378578f
Elton Sandré <eltonsantosandre@gmail.com>
2016-11-08 19:19:12 -0200

    core user

--- spring4-thymeleaf3-core/src/main/java/br/com/modulo/core/model/escola/Permissao.java
+++ spring4-thymeleaf3-core/src/main/java/br/com/modulo/core/model/escola/Permissao.java
@@ -1,20 +1,16 @@
 package br.com.modulo.core.model.escola;
 
 import java.io.Serializable;
-import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "grupo")
-public class Grupo implements Serializable {
+@Table(name = "permissao")
+public class Permissao implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
@@ -23,10 +19,6 @@ public class Grupo implements Serializable {
 	private Long id;
 
 	private String nome;
-
-	@ManyToMany
-	@JoinTable(name = "grupo_permissao", joinColumns = @JoinColumn(name = "id_grupo"), inverseJoinColumns = @JoinColumn(name = "id_permissao"))
-	private List<Permissao> permissoes;
 
 	public Long getId() {
 		return id;
@@ -42,14 +34,6 @@ public class Grupo implements Serializable {
 
 	public void setNome(String nome) {
 		this.nome = nome;
-	}
-
-	public List<Permissao> getPermissoes() {
-		return permissoes;
-	}
-
-	public void setPermissoes(List<Permissao> permissoes) {
-		this.permissoes = permissoes;
 	}
 
 	@Override
@@ -68,7 +52,7 @@ public class Grupo implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Grupo other = (Grupo) obj;
+		Permissao other = (Permissao) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
--- spring4-thymeleaf3-core/src/main/java/br/com/modulo/core/model/escola/Usuario.java
+++ spring4-thymeleaf3-core/src/main/java/br/com/modulo/core/model/escola/Usuario.java
@@ -1,74 +1,161 @@
 package br.com.modulo.core.model.escola;
 
+import java.io.Serializable;
 import java.util.Date;
+import java.util.List;
 
-public class Usuario {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import javax.validation.constraints.Size;
 
-    private int id;
-    private Date dataCadastro;
-    private boolean ativo;
-    private Funcionario funcionario;
-    private String userName;
-    private String password;
-    private boolean admin;
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotBlank;
 
-    public Usuario() {
-    }
-    
-    public int getId() {
-        return id;
-    }
+import br.com.modulo.core.validation.AtributoConfirmacao;
 
-    public void setId(int id) {
-        this.id = id;
-    }
+@AtributoConfirmacao(atributo = "senha", atributoConfirmacao = "confirmacaoSenha"
+				, message = "Confirmação da senha não confere")
+@Entity
+@Table(name = "usuario")
+@DynamicUpdate
+public class Usuario implements Serializable {
 
-    public Date getDataCadastro() {
-        return dataCadastro;
-    }
+	private static final long serialVersionUID = 1L;
 
-    public void setDataCadastro(Date dataCadastro) {
-        this.dataCadastro = dataCadastro;
-    }
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    public boolean isAtivo() {
-        return ativo;
-    }
+	@NotBlank(message = "Nome é obrigatório")
+	private String nome;
 
-    public void setAtivo(boolean ativo) {
-        this.ativo = ativo;
-    }
+	@NotBlank(message = "E-mail é obrigatório")
+	@Email(message = "E-mail inválido")
+	private String email;
 
-    public Funcionario getFuncionario() {
-        return funcionario;
-    }
+	private String senha;
 
-    public void setFuncionario(Funcionario funcionario) {
-        this.funcionario = funcionario;
-    }
+	@Transient
+	private String confirmacaoSenha;
 
-    public String getUserName() {
-        return userName;
-    }
+	private Boolean ativo;
 
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
+	@Size(min = 1, message = "Selecione pelo menos um grupo")
+	@ManyToMany
+	@JoinTable(name = "usuario_grupo", joinColumns = @JoinColumn(name = "id_usuario")
+				, inverseJoinColumns = @JoinColumn(name = "id_grupo"))
+	private List<Grupo> grupos;
 
-    public String getPassword() {
-        return password;
-    }
+	@Column(name = "data_nascimento")
+	private Date dataNascimento;
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	@PreUpdate
+	private void preUpdate() {
+		this.confirmacaoSenha = senha;
+	}
 
-    public boolean isAdmin() {
-        return admin;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setAdmin(boolean admin) {
-        this.admin = admin;
-    }
-   
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public Boolean getAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(Boolean ativo) {
+		this.ativo = ativo;
+	}
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public List<Grupo> getGrupos() {
+		return grupos;
+	}
+
+	public void setGrupos(List<Grupo> grupos) {
+		this.grupos = grupos;
+	}
+
+	public String getConfirmacaoSenha() {
+		return confirmacaoSenha;
+	}
+
+	public void setConfirmacaoSenha(String confirmacaoSenha) {
+		this.confirmacaoSenha = confirmacaoSenha;
+	}
+
+	public boolean isNovo() {
+		return id == null;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
 }
